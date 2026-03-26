afd915b776df563781b2445419171b2af8c5c746
yashhvyas <vyasyash277@gmail.com>
2026-03-26 16:55:07 +0530

    Add files via upload

---
+++ lab/simple-calculator.py
@@ -0,0 +1,50 @@
+
+
+#  # simple calculator
+# number1 = int(input("entre a first number "))
+# sing = input ("")
+# number2 = int(input("enter a second number "))
+# if sing =="+":
+#     print(int(number1 + number2))
+# elif sing =="-":
+#     print(number1 - number2)
+# elif sing =="*":
+#     print(number1 * number2)
+# elif sing =="/":
+#     print(number1/number2)
+# elif sing =="%":
+#     print( number1 % number2)
+# elif sing =="**":
+#     print(number1 ** number2)
+
+
+
+
+# # other one using function create calculetar
+# def calculator(a, b, operator):
+#     if operator == "+":
+#         return a + b
+#     elif operator == "-":
+#         return a - b
+#     elif operator == "*":
+#         return a * b
+#     elif operator == "/":
+#         if b != 0:
+#             return a / b
+#         else:
+#             return "Error: Division by zero"
+#     elif operator == "%":
+#         return a % b
+#     elif operator == "**":
+#         return a ** b
+#     else:
+#         return "Invalid operator"
+
+# # Taking user input
+# number1 = float(input("Enter the first number: "))
+# operator = input("Enter an operator (+, -, *, /, %, **): ")
+# number2 = float(input("Enter the second number: "))
+
+# # Calling the calculator function
+# result = calculator(number1, number2, operator)
+# print("Result:", result)
